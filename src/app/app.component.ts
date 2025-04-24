b3ea8e4b97de88c84de728743a5ccc8b72d70c6c
victornext <victorbrs517@gmail.com>
2025-04-24 20:20:07 -0300

    Component atraves de CLI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloWorldComponent } from './HelloWorld/Helloworld.component'
+import {PaginaComponent} from './pagina/pagina.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HelloWorldComponent],
+  imports: [RouterOutlet, HelloWorldComponent, PaginaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
