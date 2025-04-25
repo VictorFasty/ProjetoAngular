59140460b68c8c453c364f5ca1ab6358710d2141
victornext <victorbrs517@gmail.com>
2025-04-25 15:17:06 -0300

    Projeto calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloWorldComponent } from './HelloWorld/Helloworld.component'
 import {PaginaComponent} from './pagina/pagina.component'
+import {CalculadoraComponent} from './calculadora/calculadora.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HelloWorldComponent, PaginaComponent],
+  imports: [RouterOutlet, HelloWorldComponent, PaginaComponent, CalculadoraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
